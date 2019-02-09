fb816f488ed1785226e8664a634f2b9c7e3a1d40
jacques lowe <jacques.lowe@gmail.com>
2019-02-09 10:55:56 -0600

    movies api support
    added helper method, added Movies api  request

--- WebApplication1/WebApplication1/Controllers/MusicController.cs
+++ WebApplication1/WebApplication1/Controllers/MusicController.cs
@@ -9,25 +9,51 @@ namespace WebApplication1.Controllers
 {
     public class MusicController : Controller
     {
-        private string appleURI = "https://itunes.apple.com/search?";
-        private int searchLimit = 25;
-        private string entityType = "song";
+        
+       
 
-        // route example: http://localhost:49887/music/?artist=air
+        // request example: http://localhost:49887/music/?artist=air
         [HttpGet("/music")]
         [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
         [ProducesResponseType(200, Type = typeof(AppleResponse))]
-        public async Task<IActionResult> Get([FromQuery]string artist)
+        public async Task<IActionResult> GetMusic([FromQuery]string artist)
         {
             if (artist == null)
             {
                 return BadRequest();
             }
 
-            string url = appleURI +
-                   "term=" + artist +
-                   "&limit=" + searchLimit +
-                   "&entity=" + entityType;
+            string url = buildRequest( "song", artist);
+           
+            var responseString = await this.GetAsync(url);
+            if (responseString == null)
+            {
+                return NoContent();
+            }
+
+            var appleResponse = JsonConvert.DeserializeObject<AppleResponse>(responseString);
+            if (appleResponse == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(appleResponse);
+        }
+
+        // request example: http://localhost:49887/movies/?movie=avengers
+        [HttpGet("/movies")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(200, Type = typeof(AppleResponse))]
+        public async Task<IActionResult> GetMovies([FromQuery]string movie)
+        {
+            if (movie == null)
+            {
+                return BadRequest();
+            }
+
+            string url = buildRequest("movie", movie);
 
             var responseString = await this.GetAsync(url);
             if (responseString == null)
@@ -44,7 +70,16 @@ namespace WebApplication1.Controllers
             return Ok(appleResponse);
         }
 
-        public async Task<string> GetAsync(string uri)
+        private string buildRequest(string type, string term )
+        {
+            string appleURI = "https://itunes.apple.com/search?";
+            int searchLimit = 25;
+            return appleURI +
+                  "term=" + term +
+                  "&limit=" + searchLimit +
+                  "&entity=" + type;
+        }
+        private async Task<string> GetAsync(string uri)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
             request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
